833741e4ac0b772b40af72a4e8e564263f5a2b47
ismailtaher <ismail.taher@ymail.com>
2024-10-26 18:13:27 +0500

    Added Routes

---
+++ src/features/posts/SinglePostPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+import { useSelector } from "react-redux";
+import { selectPostById } from "./postsSlice";
+
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
+
+import React from "react";
+
+const SinglePostPage = () => {
+  // retrieve postId
+  const post = useSelector((state) => selectPostById(state, postId));
+
+  if (!post) {
+    return (
+      <section>
+        <h2>Post not found!</h2>
+      </section>
+    );
+  }
+
+  return (
+    <article>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <p className="postCredit">
+        <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+      </p>
+    </article>
+  );
+};
+
+export default SinglePostPage;
