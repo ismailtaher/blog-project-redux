615e57ff784018122bc88e4336692d609c1dba15
ismailtaher <ismail.taher@ymail.com>
2024-10-30 20:03:51 +0500

    Added SinglePostPage Logic

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -7,11 +7,11 @@ import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import ReactionButtons from "./ReactionButtons";
 
-import React from "react";
+import { useParams } from "react-router-dom";
 
 const SinglePostPage = () => {
-  // retrieve postId
-  const post = useSelector((state) => selectPostById(state, postId));
+  const { postId } = useParams();
+  const post = useSelector((state) => selectPostById(state, Number(postId)));
 
   if (!post) {
     return (
@@ -29,6 +29,7 @@ const SinglePostPage = () => {
         <PostAuthor userId={post.userId} />
         <TimeAgo timestamp={post.date} />
       </p>
+      <ReactionButtons post={post} />
     </article>
   );
 };
